13296c02d9a2bc40974a999c43d3a7fbae7f3428
Abdul raheem Asangi <123628985+abdulraheemasangi@users.noreply.github.com>
2026-07-15 15:13:50 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/AKmech-masar-ndt/">
+    <BrowserRouter basename="/AKmech-masar-ndt-2/">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
